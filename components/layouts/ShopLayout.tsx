225feff677b28d52888f7316b2ec48d622be760e
BreinerJT <breinerjtorres@gmail.com>
2023-01-04 12:46:57 -0500

    add ShopLayout

---
+++ components/layouts/ShopLayout.tsx
@@ -0,0 +1,32 @@
+import Head from "next/head"
+import { FC } from "react"
+
+interface Props {
+  children: JSX.Element
+  imageFullUrl?: string
+  pageDescription: string
+  title: string
+}
+
+export const ShopLayout: FC<Props> = ({ children, imageFullUrl, pageDescription, title }) => {
+  return (
+    <>
+      <Head>
+
+        <title>{ title }</title>
+        <meta name="description" content={ pageDescription } />
+        <meta name="og:title" content={ title } />
+        <meta name="og:description" content={ pageDescription } />
+        { imageFullUrl && (
+          <meta name="og:image" content={ imageFullUrl } />
+        )}
+
+      </Head>
+
+      <main className="px-24">
+        { children }
+      </main>
+
+    </>
+  )
+}
