2bcad9be2ae4429e4e7093e3264c153716b1a002
BreinerJT <breinerjtorres@gmail.com>
2023-01-06 01:48:19 -0500

    fix icons warnings

--- components/ui/Strip.tsx
+++ components/ui/Strip.tsx
@@ -1,5 +1,6 @@
-import { CiLocationOn, CiDeliveryTruck } from 'react-icons/ci'
+import { BsTruck } from 'react-icons/bs'
 import { BiBadgeCheck } from 'react-icons/bi'
+import { IoLocationOutline } from 'react-icons/io5'
 
 
 export const Strip = () => {
@@ -8,14 +9,14 @@ export const Strip = () => {
       <p className='text-sm'>Welcome to worldwide MegaMart!</p>
       <div className='flex gap-4'>
         <div className='flex gap-2 items-center'>
-          <CiLocationOn className='text-primary' size={20} />
+          <IoLocationOutline className='text-primary' size={20} />
           <p className='text-sm'>Deliver to <strong>423651</strong></p>
         </div>
 
         <div className='border border-transparent border-r-gray-300' />
 
         <div className='flex gap-2 items-center'>
-          <CiDeliveryTruck className='text-primary' size={20} />
+          <BsTruck className='text-primary' size={20} />
           <p className='text-sm'>Track your order</p>
         </div>
 
